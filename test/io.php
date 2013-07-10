8bc397f149e8172d6aab87af28e4e56701914abb
wang zhiyong <36891314@163.com>
2013-07-10 09:34:48 +0800

    111

---
+++ test/io.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Administrator
+ * Date: 13-6-27
+ * Time: ÉÏÎç8:49
+ * To change this template use File | Settings | File Templates.
+ */
+header('Content-type:application/json');
+$response = array();
+if(isset($_GET['SUSPECT']) && $_GET['suspect'] === 'butler'){
+    $response['guilty'] = 'YES';
+    $response['comment'] = 'We are as shocked as you are';
+}
+else{
+    $response['guilty'] = 'NO';
+    $response['comment'] = 'Perhaps this murder will never be solved.';
+}
+echo json_encode($response);
+?>
